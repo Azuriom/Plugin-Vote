0f4f2c8e45f23c904257ce14d87112411bad2c2c
Maxence <maxence.simon@tagandplay.com>
2021-12-01 10:42:56 +0100

    :construction: Add test verif header

--- src/Sites/ServeurMinecraftVoteWebhook.php
+++ src/Sites/ServeurMinecraftVoteWebhook.php
@@ -2,17 +2,40 @@
 
 namespace Azuriom\Plugin\Vote\Sites;
 
+use Azuriom\Plugin\Vote\Controllers\Admin\ServeurMinecraftVoteController;
 use Illuminate\Http\Request;
+use ServeurMinecraftVote\Exceptions\SignatureVerificationException;
 use ServeurMinecraftVote\ServeurMinecraftVote;
 
 class ServeurMinecraftVoteWebhook
 {
 
-    public function webhook(Request $request){
+    /**
+     * @throws SignatureVerificationException
+     */
+    public function webhook(Request $request): string
+    {
 
         $smv = new ServeurMinecraftVote();
 
-        $smv->verifyHeader();
+        $key = setting(ServeurMinecraftVoteController::SETTINGS_WEBHOOK);
+
+        if (empty($key)) {
+            return "Impossible to recover the secret key of the webhook.";
+        }
+
+        $header = $request->header('X-SMV-Signature');
+        $data = json_encode($request->all());
+        $smv->verifyHeader($data, $header, $key);
+
+        $type = $request['type'];
+
+        switch ($type) {
+            case "user.follow":
+
+                break;
+
+        }
 
         return "OK";
     }
