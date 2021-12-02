e87e058b08e33932969757b52f2551e2da1d5a73
Maxence <maxence.simon@tagandplay.com>
2021-12-02 11:26:56 +0100

    :construction: Move Class to ApiController

--- src/Controllers/Api/ApiController.php
+++ src/Controllers/Api/ApiController.php
@@ -3,9 +3,15 @@
 namespace Azuriom\Plugin\Vote\Controllers\Api;
 
 use Azuriom\Http\Controllers\Controller;
+use Azuriom\Plugin\Vote\Controllers\Admin\Sites\ServeurMinecraftVoteController;
+use Azuriom\Plugin\Vote\Models\WebhookHistory;
+use Azuriom\Plugin\Vote\Models\WebhookReward;
 use Azuriom\Plugin\Vote\Sites\ServeurMinecraftVoteWebhook;
 use Azuriom\Plugin\Vote\Verification\VoteChecker;
+use Exception;
 use Illuminate\Http\Request;
+use ServeurMinecraftVote\Exceptions\SignatureVerificationException;
+use ServeurMinecraftVote\ServeurMinecraftVote;
 
 class ApiController extends Controller
 {
@@ -17,12 +23,69 @@ class ApiController extends Controller
         return $verifier->executePingbackCallback($request) ?? response()->noContent();
     }
 
-    public function webhooks(Request $request, string $site){
-        if ($site === 'smv'){
-            $smv = new ServeurMinecraftVoteWebhook();
-            return $smv->webhook($request);
+    /**
+     * @param Request $request
+     * @param string $site
+     * @return string
+     * @throws Exception
+     */
+    public function webhooks(Request $request, string $site)
+    {
+
+        if ($site !== 'smv') {
+            return json_encode([
+                'status' => 'error',
+                'message' => 'Website not found',
+            ]);
         }
-        return "Website not found";
+
+        try {
+
+
+            $smv = new ServeurMinecraftVote();
+
+            $key = setting(ServeurMinecraftVoteController::SETTINGS_WEBHOOK);
+
+            if (empty($key)) {
+                return json_encode([
+                    'status' => 'error',
+                    'message' => 'Impossible to recover the secret key of the webhook.',
+                ]);
+            }
+
+            $header = $request->header('X-SMV-Signature');
+            $jsonData = $request['data'];
+            $jsonData = is_array($jsonData) ? json_encode($jsonData) : $jsonData;
+
+            //   $smv->verifyHeader($jsonData, $header, $key);
+
+            $type = $request['type'];
+            $data = $request['data'];
+            $reward = WebhookReward::getRandomReward($type, $data['user']['name'] ?? '');
+
+            if (empty($reward)) {
+                return "No reward found for $type";
+            }
+            
+            $reward->giveTo($data['user']['name'] ?? '');
+
+            if ($reward->limit !== 0) {
+                WebhookHistory::create([
+                    'webhook_reward_id' => $reward->id,
+                    'name' => $data['user']['name'] ?? '',
+                ]);
+            }
+
+            return json_encode([
+                'status' => 'success',
+                'message' => 'OK',
+            ]);
+        } catch (SignatureVerificationException $exception) {
+            return json_encode([
+                'status' => 'error',
+                'message' => $exception->getMessage(),
+            ]);
+        }
     }
 
 }
