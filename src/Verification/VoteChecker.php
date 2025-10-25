58a486303d069488dc0bf1ac20c9301cbeb997d8
Tristan Richard <richard.tristan.93@gmail.com>
2025-10-25 09:04:44 +0200

    fix return value

--- src/Verification/VoteChecker.php
+++ src/Verification/VoteChecker.php
@@ -124,7 +124,7 @@ class VoteChecker
         $this->register(VoteVerifier::for('server-pulse.net')
             ->setApiUrl('https://server-pulse.net/api/servers/{server}/vote-status/{ip}')
             ->requireKey('token')
-            ->verifyByJson('success', 200));
+            ->verifyByJson('success', true));
 
         $this->register(VoteVerifier::for('top-serveurs.net')
             ->setApiUrl('https://api.top-serveurs.net/v1/votes/check-ip?server_token={server}&ip={ip}')
